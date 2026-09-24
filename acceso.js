6c1094322ab3defc832b79d1a4b30214bd0f8de1
GOMA <mg5539891@gmail.com>
2026-09-24 15:28:36 -0500

    actualizar logica de intentos

--- acceso.js
+++ acceso.js
@@ -1,6 +1,24 @@
-function acceso() {
+function acceso()
 
     const USUARIO_CORRECTO = "admin";
     const CONTRSEÑA_correcta = "123"
-    
-}
+
+    const MAX_INTENTOS = 3;
+
+    let intentos = 0;
+    let accesoConcedido = false;
+
+    while (intentos < MAX_INTENTOS && !accesoConcedido) 
+        intentos++;
+
+        const usuarioIngresado = prompt(`Intento ${intentos} de ${MAX_INTENTOS}\nIngrese su usuario:`);
+        const contraseñaIngresada = prompt(`Intento ${intentos} de ${MAX_INTENTOS}\nIngrese su contraseña:`);
+
+        if (usuarioIngresado === USUARIO_CORRECTO && contraseñaIngresada === CONTRASEÑA_CORRECTA) {
+            accesoConcedido = true;
+            console.log("¡Bienvenido al sistema!");
+            } else {
+            if (intentos < MAX_INTENTOS) {
+                console.log(`Datos incorrectos. Intento ${intentos} de ${MAX_INTENTOS}. Intente nuevamente.`);
+            }
+    }
